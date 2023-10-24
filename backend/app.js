ae773e99a55e51d9ceb3df087ca0e61288296559
Underemployed <underemployed871@gmail.com>
2023-10-24 17:58:45 +0530

    Dashboard,Income,Expense Completed

--- backend/app.js
+++ backend/app.js
@@ -7,7 +7,7 @@ const { db } = require("./db/db");
 
 // Create an Express application
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
 
 // Middlewares
 app.use(express.json())
